0b144909fb49e128fc4fcebb705ccedd52d7f666
Ricardo <jew16373@educastur.es>
2022-03-25 13:13:08 +0100

    Clase GeneradorPrincipal terminada

---
+++ src/GeneradorIp.java
@@ -0,0 +1,20 @@
+import java.util.Random;
+
+public class GeneradorIp {
+	Random random = new Random();
+
+	public int generarNumero(int min, int max) {
+		return random.nextInt(max - min + 1) + min;
+	}
+
+	public String generarIp() {
+		StringBuilder sb = new StringBuilder();
+		for (int i = 0; i < 3; i++) {
+			sb.append(generarNumero(1, 256));
+			sb.append(":");
+		}
+		sb.append(generarNumero(1, 256));
+
+		return sb.toString();
+	}
+}
